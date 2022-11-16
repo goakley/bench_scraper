2da4fe4ba3179daf97f96545e77d150795b28118
Glen Oakley <glen.oakley@discordapp.com>
2022-11-16 01:17:27 +0000

    Add example

---
+++ examples/csv.rs
@@ -0,0 +1,21 @@
+use bench_scraper::find_cookies;
+
+fn main() {
+    let mut wtr = csv::Writer::from_writer(std::io::stdout());
+    let browser_cookies = find_cookies().unwrap();
+    for browser_cookie in browser_cookies.iter() {
+        let browser_name = format!("{:?}", browser_cookie.browser);
+        for cookie in browser_cookie.cookies.iter() {
+            wtr.write_record(&[
+                &browser_name,
+                &cookie.host,
+                &cookie.path,
+                &cookie.name,
+                &cookie.value,
+                &format!("{:?}", cookie.is_secure),
+                &format!("{:?}", cookie.is_http_only),
+                &format!("{:?}", cookie.last_accessed),
+            ]).unwrap();
+        }
+    }
+}
